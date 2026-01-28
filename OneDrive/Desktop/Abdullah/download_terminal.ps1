722f854bf4fb913f864a1587cae07b7acaa72a58
Abdullah Masood <imabd645@gmail.com>
2026-01-28 21:53:47 +0500

    V5

--- OneDrive/Desktop/Abdullah/download_terminal.ps1
+++
@@ -1,6 +0,0 @@
-try {
-    Invoke-WebRequest -Uri 'https://img.icons8.com/color/48/000000/console.png' -OutFile 'assets\icons\terminal.png' -ErrorAction Stop
-    Write-Host "Download Complete"
-} catch {
-    Write-Host "Error downloading: $_"
-}
